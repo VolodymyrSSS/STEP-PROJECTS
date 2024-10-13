d8d6cdd7f483607af12df70346113e41be313b2d
Volodymyr Sych <volodymyr_sych@epam.com>
2024-10-13 13:12:13 +0300

    deleting the notes

--- step-project-8-Like-Google-keep/js/main.js
+++ step-project-8-Like-Google-keep/js/main.js
@@ -4,64 +4,101 @@ const noteTitleInput = document.querySelector('#noteTitleInput');
 const noteTextInput = document.querySelector('#noteTextInput');
 const formTextRemovable = document.querySelector('.form__text-removable');
 const cardsWrapper = document.getElementsByClassName('main__cards-wrapper')[0]; // якщо використовується метод "getElementsByClassName" то повертається масив значень а тому потрібно додавати на той момент єдине значення масиву під індексом "[0]"
-// const formTextRemovable = document.querySelector('.form__text-removable');
+const noNotesMessage = document.getElementsByClassName('no-notes-message')[0];
 
-// створюємо початковий масив нотаток
-const notes = [
+// створюємо початковий масив нотаток але через "let" бо можемо застосовувати методи які можуть модифікувати оригінальний масив типу: .filter(), .sort() і т.д.
+let notes = [
 	{
+		id: 1,
 		title: 'Основи JS',
 		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consectetur hendrerit elementum.',
 	},
 	{
+		id: 2,
 		title: 'DOM-дерево',
 		text: 'Sed tristique quam sed tempus pretium. Ut et volutpat nisl, vitae suscipit arcu.',
 	},
 	{
+		id: 3,
 		title: 'Події в JS',
 		text: 'Sed rutrum, orci at luctus varius, lacus turpis porta dui, quis pretium sem odio eget mi. Pellentesque ut faucibus elit.',
 	},
 ];
 
+function checkNotesEmpty() {
+	if (notes.length === 0) {
+		noNotesMessage.style.display = 'block'; // Показуєм повідомлення
+	} else {
+		noNotesMessage.style.display = 'none'; // Сховане повідомлення
+	}
+}
+
+// Call this function on initial load as well
+document.addEventListener('DOMContentLoaded', checkNotesEmpty);
+
 // слухаємо за змінами в формі
 addNodeForm.addEventListener('submit', (event) => {
 	event.preventDefault(); // відміняєм стандартну відправку форми
+
+	// додаємо нотатку
 	notes.push({
+		id: notes[notes.length - 1]['id'] + 1,
 		title: noteTitleInput.value,
 		text: noteTextInput.textContent,
-	}); // додаємо нотатку
+	});
 
 	noteTitleInput.value = ''; // очищаэмо поле вводу заголовку нотатки
 	noteTextInput.textContent = ''; // очищаємо поле вводу тексту нотатки
 	formTextRemovable.textContent = 'The text of the note'; // повертаєм попередній placeholder
-
 	noteTitleInput.focus(); // повертаєм фокус
 
 	const lastNote = notes[notes.length - 1]; // визначаємо останній доданий елемент масиву
-	// на сторінку додаєм останню додану нотатку на початок
+	// визначаєм як виглядає останній доданий елемент та куда його додавати на сторінку
 	const elem = `<section class="card text-bg-warning mb-3" style="max-width: 18rem;">
 						<div class="card-body">
 							<h5 class="card-title">${lastNote.title}</h5>
 							<p class="card-text">
 								${lastNote.text}
 							</p>
+							<button type="button" class="btn btn-outline-danger btn-sm float-end" data-action="delete" data-id="${lastNote.id}">Delete</button>
 						</div>
 					</section>`;
 
 	cardsWrapper.insertAdjacentHTML('afterbegin', elem);
 });
 
-// визначаєм як додавати на сторінку html-розмічену нотатку в потрібному місці
-notes.forEach((item) => {
-	const elem = `<section class="card text-bg-warning mb-3" style="max-width: 18rem;">
+// визначаєм як виглядатимуть нотатки та куда їх додавати на сторінку
+notes.forEach((item, id) => {
+	const elem = `<section class="card text-bg-warning mb-2" style="max-width: 18rem;">
 						<div class="card-body">
 							<h5 class="card-title">${item.title}</h5>
 							<p class="card-text">
 								${item.text}
 							</p>
+							<button type="button" class="btn btn-outline-danger btn-sm float-end" data-action="delete" data-id="${id}">Delete</button>
 						</div>
 					</section>`;
 
 	cardsWrapper.insertAdjacentHTML('afterbegin', elem);
 });
 
-// https://www.youtube.com/watch?v=gooOJGDfmt4&ab_channel=%D0%92%D0%B5%D0%B1%D0%9A%D0%B0%D0%B4%D0%B5%D0%BC%D0%B8%7C%D0%AE%D1%80%D0%B8%D0%B9%D0%9A%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D1%81%D0%BA%D0%B8%D0%B9
+// видалення нотатки зі сторінки
+document.addEventListener('click', (event) => {
+	console.log(event.target);
+
+	if (event.target.dataset.action === 'delete') {
+		const id = event.target.dataset.id; // уважно! тут отримаємо рядковий символ а не число
+
+		// видаляємо нотатку з масиву по індексу, що не змінює оригінальний масив
+		const deletedItemIndex = notes.findIndex((item) => item.id == id); // нестроге порівняння бо тут "id" може бути текстовим символом а "item.id" є число
+		notes.splice(deletedItemIndex, 1);
+
+		// альтернативне видалення нотаток з масиву через .filter(), який змінює ориг масив
+		// notes = notes.filter((item) => item.id != id); // нестроге порівняння рядка і числа
+
+		// видаляємо нотатку зі сторінки
+		event.target.closest('.card').remove();
+
+		checkNotesEmpty();
+	}
+});
